a7cd690e7beaf70539cf2df4c21d725247122313
segundo <segujuan27@hotmail.com>
2024-10-19 01:44:40 +0100

    responsive layout

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,14 +1,33 @@
+import { useState } from "react"
 import { useAuth } from "../context/useAuth"
 import "../styles/css/style.css"
 import { Outlet, Link } from "react-router-dom"
+import { IoMenu } from "react-icons/io5"
+import { IoIosCloseCircleOutline } from "react-icons/io"
 
 export default function Sidebar() {
   const { user, logout } = useAuth()
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
 
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen)
+  }
   return (
-    <>
-      <div className="sidebar">
-        <h1>wise</h1>
+    <div className="layout-container">
+      <header>
+        <button onClick={toggleSidebar} className="link">
+          <IoMenu size={30} />
+        </button>
+      </header>
+
+      <div className={`sidebar ${isSidebarOpen ? "active" : ""}`}>
+        <div className="sidebar-header">
+          <h1>Wais</h1>
+          <button className="link" onClick={toggleSidebar}>
+            <IoIosCloseCircleOutline size={30} />
+          </button>
+        </div>
+
         <nav className="nav">
           <ul>
             <li>
@@ -26,9 +45,10 @@ export default function Sidebar() {
           )}
         </nav>
       </div>
+
       <div className="detail">
         <Outlet />
       </div>
-    </>
+    </div>
   )
 }

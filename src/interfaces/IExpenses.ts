ece7d2ef477b769566e934f0a68bf746a6e4250a
segundo <segujuan27@hotmail.com>
2024-10-20 03:07:14 +0100

    replaced description field for date

--- src/interfaces/IExpenses.ts
+++ src/interfaces/IExpenses.ts
@@ -1,14 +1,24 @@
-export interface Expense {
+export interface IExpense {
   id: number
   amount: number
   category: Category
-  description: string
+  date: string
   type: "in" | "out" // (income or expense)
 }
 
 export type Category =
+  | "Salary"
   | "Food"
   | "Transportation"
   | "Entertainment"
   | "Utilities"
   | "Others"
+
+export const categories: Category[] = [
+  "Salary",
+  "Food",
+  "Transportation",
+  "Entertainment",
+  "Utilities",
+  "Others",
+]
